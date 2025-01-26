faa68cec9640bf36a691fc0d4d8617b42a65b356
Your Name <sacralprojects8@gmail.com>
2025-01-26 10:45:41 +0300

    55

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,22 @@ import { useEffect } from "react";
 import '../styles/globals.css'; 
 
 function MyApp({ Component, pageProps }) {
-  
+  useEffect(() => {
+    if (typeof window !== 'undefined') { // Проверяем, что код выполняется на клиенте
+      const threeScript = document.createElement("script");
+      threeScript.setAttribute("id", "threeScript");
+      threeScript.setAttribute(
+        "src",
+        "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+      );
+      document.getElementsByTagName("head")[0].appendChild(threeScript);
+      return () => {
+        if (threeScript) {
+          threeScript.remove();
+        }
+      };
+    }
+  }, []);
 
   return <Component {...pageProps} />;
 }
